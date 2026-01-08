3083d76f35b3986b542e20bcb69329e0263a9465
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2026-01-08 22:41:12 +0530

    table with external api data

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { AgGridReact } from "ag-grid-react";
-import { ModuleRegistry, AllCommunityModule, TooltipModule } from "ag-grid-community";
+import {
+  ModuleRegistry,
+  AllCommunityModule,
+  TooltipModule,
+} from "ag-grid-community";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
@@ -8,39 +12,53 @@ import "ag-grid-community/styles/ag-theme-quartz.css";
 ModuleRegistry.registerModules([AllCommunityModule, TooltipModule]);
 
 export const Table = () => {
-  const rowData = [
-    { name: "arnab", age: 22, birthYear: 2000 },
-    { name: "aman", age: 14, birthYear: 2002 },
-    { name: "ankit", age: 32, birthYear: 2008 },
-  ];
+//   const rowData = [
+//     { name: "arnab", age: 22, birthYear: 2000 },
+//     { name: "aman", age: 14, birthYear: 2002 },
+//     { name: "ankit", age: 32, birthYear: 2008 },
+//   ];
 
   const columnDefs = [
     {
+      headerName: "ID",
+      field: "id",
+      tooltipField: "name",
+    },
+      {
       headerName: "Name",
       field: "name",
       tooltipField: "name",
     },
-    {
-      headerName: "Age",
-      field: "age",
-      tooltipField: "name", // you can show age too
-      cellStyle: (params) => ({
-        backgroundColor: params.value > 18 ? "lightgreen" : "lightcoral",
-        color: "black",
-        fontWeight: "600",
-      }),
+      {
+      headerName: "Email",
+      field: "email",
     },
-    { headerName: "Birth Year", field: "birthYear", tooltipField: "name" },
+      {
+      headerName: "Body",
+      field: "body",
+    },
+      {
+      headerName: "PostId",
+      field: "postId",
+    },
+   
   ];
+
+  const onGridReady = (params) => {
+    fetch("https://jsonplaceholder.typicode.com/comments")
+      .then((resp) => resp.json())
+      .then((resp) => { console.log(resp)
+         params.api.applyTransaction({ add: resp })});
+  };
 
   return (
     <div className="ag-theme-quartz" style={{ height: 400 }}>
       <AgGridReact
-        rowData={rowData}
         columnDefs={columnDefs}
         defaultColDef={{ sortable: true, filter: true, flex: 1 }}
         enableBrowserTooltips={true}
-        tooltipShowDelay={{tooltipShowDelay:2}}
+        onGridReady={onGridReady}
+        tooltipShowDelay={{ tooltipShowDelay: 2 }}
       />
     </div>
   );
